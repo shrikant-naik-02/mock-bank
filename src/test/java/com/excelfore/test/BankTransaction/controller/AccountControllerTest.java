7d79019fb64fb416be5afc68fa67fac590c40e45
shrikant-Naik-02 <shrikantpramodnaik@example.com>
2025-05-07 17:28:51 +0530

    Implement Basic Structure Of Junit For Controller

---
+++ src/test/java/com/excelfore/test/BankTransaction/controller/AccountControllerTest.java
@@ -0,0 +1,46 @@
+package com.excelfore.test.BankTransaction.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createAccount() {
+    }
+
+    @Test
+    void getSingleAccount() {
+    }
+
+    @Test
+    void getAllAccount() {
+    }
+
+    @Test
+    void deposit() {
+    }
+
+    @Test
+    void withdraw() {
+    }
+
+    @Test
+    void deleteAccount() {
+    }
+
+    @Test
+    void myTest() {
+    }
+}
