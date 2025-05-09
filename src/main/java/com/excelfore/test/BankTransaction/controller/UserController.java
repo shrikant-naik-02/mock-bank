72d1a94ea5a8183e4e78687ea019d51645e28e78
shrikant-Naik-02 <shrikantpramodnaik@example.com>
2025-05-09 13:54:50 +0530

    Created One Response For User Controller

--- src/main/java/com/excelfore/test/BankTransaction/controller/UserController.java
+++ src/main/java/com/excelfore/test/BankTransaction/controller/UserController.java
@@ -1,9 +1,6 @@
 package com.excelfore.test.BankTransaction.controller;
 
 import com.excelfore.test.BankTransaction.model.User;
-import com.excelfore.test.BankTransaction.service.UserService;
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -11,25 +8,36 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @Validated
 public class UserController {
 
-    @Autowired
-    private UserService userService;
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
+//    @PostMapping("/register")
+//    public ResponseEntity<String> registerUser(@RequestBody User user) {
+//        log.info("Received request to /register with params: {}", user);
+//
+//        String customJson = """
+//            {"message": "User %s registered successfully"}
+//            """.formatted(user.getUsername()); // Change to appropriate field
+//
+//        log.info(customJson);
+//        return ResponseEntity.status(HttpStatus.CREATED).body(customJson);
+//    }
 
     @PostMapping("/register")
-    public ResponseEntity<String> registerUser(@Valid @RequestBody User user) {
+    public ResponseEntity<String> registerUser(@RequestBody User user) {
+        log.info("Received request to /register with params: {}", user);
 
-        if (user == null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body("Request body cannot be null");
-        }
-        userService.createUser(user);
+//        String responseMessage = "User " + user.getUsername() + " registered successfully";
+        String responseMessage = "User registered successfully";
 
-        return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully");
+        log.info(responseMessage);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseMessage);
     }
 }
