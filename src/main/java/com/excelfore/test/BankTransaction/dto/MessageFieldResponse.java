d360a4204bfcbc915c77b267aabbf0d3afdcbda9
shrikant-Naik-02 <shrikantpramodnaik@example.com>
2025-05-13 10:37:09 +0530

    One DTO Created For Response Of Negative Field Mainly For Account Controller

---
+++ src/main/java/com/excelfore/test/BankTransaction/dto/MessageFieldResponse.java
@@ -0,0 +1,21 @@
+package com.excelfore.test.BankTransaction.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+@Schema(description = "A Simple And Clear response message")
+@Getter
+@Setter
+public class MessageFieldResponse {
+
+    @Schema(description = "The response message", example = "Account 50 not found")
+    private String message;
+
+    public MessageFieldResponse() {}
+
+    public MessageFieldResponse(String message) {
+        this.message = message;
+    }
+
+}
